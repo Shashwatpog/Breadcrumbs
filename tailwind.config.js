1f51d119ec1f08e832159e618ee6efc1726c0915
Shashwat <mishras8@mail.uc.edu>
2024-06-07 01:59:00 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}",
+            "./pages/**/*.{js,ts,jsx,tsx}",
+            "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      mycolor1: "#5f574f"
+    },
+  },
+  plugins: [],
+}
+
