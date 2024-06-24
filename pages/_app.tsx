c25c222199f8316b225dac3d5d5913a042f5f0fb
Shashwat <mishras8@mail.uc.edu>
2024-06-24 02:55:31 -0400

    Added Modal layout feature for posting on the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import Modal from '@/components/Modal';
+
 import Layout from '@/components/Layout';
 import "@/styles/globals.css";
 
@@ -8,7 +8,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal />
+
       <Layout>  
         <Component {...pageProps} />
       </Layout>
