6c9f0085bd4144b3c9310e6c7004e7a5a8843ba9
Shashwat <mishras8@mail.uc.edu>
2024-06-25 00:05:25 -0400

    Created database schema using prisma and connected MongoDB

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <Header label = "Bread Base" />
+      <Header label = "Bread Oven" />
     </>
   )
 }
